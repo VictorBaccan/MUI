f5327807e06d2f71728fa3d53da93194f32a094f
VictorBaccan <victorbaccan@outlook.com>
2023-05-13 19:55:07 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/header";
+import Login from "./pages/Login";
+import ListarTarefa from "./pages/tarefa/ListarTarefa"; 
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
